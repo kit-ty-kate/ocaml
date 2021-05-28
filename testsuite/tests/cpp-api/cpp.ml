9b1c3dd405320c6002eb2715eb40a797575a9d40
Kate <kit.ty.kate@disroot.org>
2021-05-28 17:19:30 +0100

    tmp

--- testsuite/tests/cpp-api/cpp.ml
+++ testsuite/tests/cpp-api/cpp.ml
@@ -1,12 +1,10 @@
 (* TEST
-   modules = "cpp_stubs1.cpp cpp_stubs2.cpp"
+   modules = "cpp_stubs1.cpp"
    flags = "-cc c++"
 *)
 
 external test1 : int -> unit = "test_cpp1"
-external test2 : int -> unit = "test_cpp2"
 
 let () =
   test1 2;
-  test2 6;
   print_endline "C++ api tests succeeded"
--- testsuite/tests/cpp-api/cpp_stubs1.cpp
+++ testsuite/tests/cpp-api/cpp_stubs1.cpp
@@ -9,6 +9,7 @@ value test_cpp1(value v)
   CAMLparam1(v);
 
   printf("In test C++ 1, v = %d\n", Int_val(v));
+  fflush(stdout);
 
   CAMLreturn (Val_unit);
 }
